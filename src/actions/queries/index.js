72557c67f7def5ec26a51512b6d55f29eb3e13a9
Tuanlq109 <obedientdev@gmail.com>
2020-12-15 00:07:56 +0700

    bind recipe detail

--- src/actions/queries/index.js
+++ src/actions/queries/index.js
@@ -1,9 +1,11 @@
 import homeQuery from "./homePage"
 import categoryQuery from "./category"
 import productQuery from "./product"
+import recipeQuery from "./recipe"
 
 export default {
   homeQuery,
   categoryQuery,
-  productQuery
+  productQuery,
+  recipeQuery,
 }
--- src/extensions/routes.js
+++ src/extensions/routes.js
@@ -3,13 +3,13 @@ import { enumType } from "../constants"
 const titleRoutes = {
   HOME: "Nguyên liệu Trendy",
   PRODUCT: "Sản phẩm",
-  RECIPE: "Công thức"
+  RECIPE: "Công thức",
 }
 
 const pathRoutes = {
   HOME: "/",
   PRODUCT: "/san-pham",
-  RECIPE: "cong-thuc"
+  RECIPE: "cong-thuc",
 }
 
 export const generateTitleBreadcrumb = (option) => {
@@ -26,7 +26,6 @@ export const generateTitleBreadcrumb = (option) => {
 }
 
 export const generatePath = (option) => {
-
   switch (option) {
     case enumType.categoryType.HOME:
       return pathRoutes.HOME
@@ -39,9 +38,12 @@ export const generatePath = (option) => {
   }
 }
 
-// export const getIdByUrl = (url) => {
-//   if (!url) return null
-//   const arrayStringUrl = url.split('-')
-//   const idUrl = arrayStringUrl.slice(arrayStringUrl.length - 1, arrayStringUrl.length)
-//   return idUrl
-// }
+export const getIdByUrl = (url) => {
+  if (!url) return null
+  const arrayStringUrl = url.split("-")
+  const idUrl = arrayStringUrl.slice(
+    arrayStringUrl.length - 1,
+    arrayStringUrl.length
+  )
+  return idUrl
+}
