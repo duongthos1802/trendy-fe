95e1c52cd1e3a4aa91e9400ddccf31dc49baecd6
HongDuong <hongduong@HongDuongs-MacBook-Pro.local>
2020-12-17 21:54:22 +0700

    fix img

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
 export { default as enumType } from './enum'
 export const MAX_LENGTH_DESCRIPTION = 20
-export const UPLOADS_IMAGE_URL = `http://localhost:5000/uploads`
+export const UPLOADS_IMAGE_URL = `https://api.nguyenlieutrendy.com/uploads`
 export const DEFAULT_PAGE_SIZE = 9
