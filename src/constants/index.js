94fcc6aa6b8804f8835256568694bea346f34de2
duongth52 <hongduong@HongDuongs-MacBook-Pro.local>
2021-03-24 17:56:40 +0700

    fix seo

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,5 +3,5 @@ export const MAX_LENGTH_DESCRIPTION = 20
 export const UPLOADS_IMAGE_URL = `https://api.nguyenlieutrendy.com/uploads`
 export const DEFAULT_PAGE_SIZE = 12
 export const DEFAULT_SITE = "Nguyenlieutrendy.com"
-export const DEFAULT_HTTP_SITE = "http://nguyenlieutrendy.com"
-export const DEFAULT_IMAGE_SITE = require('../static/assets/images/default/seo.jpeg')
+export const DEFAULT_HTTP_SITE = "https://nguyenlieutrendy.com"
+export const DEFAULT_IMAGE_SITE = "https://api.nguyenlieutrendy.com/uploads/banner_pictures/1616035102665-banner-trendy.jpg"
