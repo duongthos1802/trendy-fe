74c19b0de9676ecd9b6bd942530063b487cfb22e
duongth52 <hongduong@HongDuongs-MacBook-Pro.local>
2021-03-24 17:26:34 +0700

    fix img

--- src/pages/gioi-thieu/index.js
+++ src/pages/gioi-thieu/index.js
@@ -10,20 +10,20 @@ const Introduce = () => {
         <div className='row container-wrap'>
           <div className='col-sm-12 col-md-6 '>
             <div className='main-content'>
-              <h2>Trendy - Nguyên liệu pha chế được tin dùng bởi Baristas</h2>
+              <h2>Trendy - Nguyên liệu pha chế được tin dùng bởi Barista</h2>
               <p className="text-justify">
                 Ngành công nghiệp pha chế đồ uống ngày càng phát triển, đòi hỏi
                 sự ra đời của những hương vị mới và sáng tạo hơn nữa trong
                 nguyên liệu và cách pha chế. Bằng cách lắng nghe và thấu hiểu
                 nhu cầu của khách hàng, Trendy ra đời với sứ mệnh đem lại những
                 sản phẩm phù hợp với việc kinh doanh và pha chế đồ uống, đặc
-                biệt được tin dùng bởi các Baristas đầu ngành. Mỗi sản phẩm của
+                biệt được tin dùng bởi các Barista đầu ngành. Mỗi sản phẩm của
                 Trendy đều được tỉ mỉ và cẩn thận trong từng khâu thiết kế, chế
                 biến, đóng gói và vận chuyển, mang đến hương vị của thiên nhiên
                 cùng sự tin tưởng về những ly đồ uống ngon sắp được ra đời.
                 Chúng tôi tin rằng, với năng lượng tích cực mà Trendy mang lại,
                 mỗi ngày pha chế sẽ là một ngày vui cùng đam mê của các
-                Baristas, mỗi sản phẩm từ nguyên liệu của Trendy sẽ là một món
+                Barista, mỗi sản phẩm từ nguyên liệu của Trendy sẽ là một món
                 quà tới người thưởng thức.
               </p>
               {/* <p>
@@ -39,7 +39,7 @@ const Introduce = () => {
       </div>
       <div className='content-para'>
         <h2>Trendy - Đồng hành để phát triển</h2>
-        <p  className="text-justify">
+        <p className="text-justify">
           Trendy tự hào mang đến các dòng nguyên liệu pha chế bao gồm: Syrup,
           Bột, Topping,... Trong tương lai, danh mục sản phẩm của Trendy sẽ tiếp
           tục được nghiên cứu để mở rộng, phát triển về cả số lượng và chất
