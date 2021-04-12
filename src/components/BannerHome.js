4060d6ef8428089a4f88665f0a1a8b0c65ae7b0c
duongth <duongth@thnaglongtcd.com>
2021-04-12 11:17:29 +0700

    fix banner

--- src/components/BannerHome.js
+++ src/components/BannerHome.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { imageUtils } from '../utils'
 import { enumType } from '../constants'
+import className from 'classnames'
 
 const BannerHome = ({ banner }) => {
 
@@ -10,46 +11,28 @@ const BannerHome = ({ banner }) => {
       window.open(`${url}`, '_blank')
     }
   }
-
-  return (
-    <div className="banner_section staggered-animation-wrap">
-      <div id="carouselExampleControls" className="carousel slide carousel-fade carousel_style1 light_arrow" data-ride="carousel">
+  // .cursor-pointer 
+  return banner && (
+    <div className="banner_section-mobile staggered-animation-wrap banner-fixed">
+      <div id="carouselExampleControls" className="carousel slide carousel-fade carousel_style2 light_arrow" data-ride="carousel">
         <div className="carousel-inner">
-          {
-            banner
-              ? banner.map((item, i) => (
-                <div
-                  className={"carousel-item background_bg cursor-pointer " + (i === 0 ? " active" : "")}
-                  style={{ backgroundImage: `url(${imageUtils.getBlockUrl(item.image, enumType.imagePath.Banner)})` }}
-                  onClick={(e) => handleClick(e, item.url)}
-                >
-                  <div className="banner_slide_content">
-                    <div className="container">
-                      {/* STRART CONTAINER */}
-                      <div className="row justify-content-center">
-                        <div className="col-lg-7 col-md-12 col-sm-12 text-center">
-                          {/* <div className="banner_content text_white">
-                        <h2 className="staggered-animation font_style1" data-animation="fadeInUp" data-animation-delay="0.2s">Delicious food</h2>
-                        <p className="staggered-animation" data-animation="fadeInUp" data-animation-delay="0.4s">If you are going to use a passage of Lorem middle of text.</p>
-                        <a className="btn btn-default btn-radius staggered-animation" href="#" data-animation="fadeInUp" data-animation-delay="0.6s">Get Started</a>
-                        <a className="btn btn-white btn-radius staggered-animation" href="#" data-animation="fadeInUp" data-animation-delay="0.6s">Learn More</a>
-                      </div> */}
-                        </div>
-                      </div>
-                    </div>{/* END CONTAINER*/}
-                  </div>
-                </div>
-              ))
-              : null
-
-          }
-
+          {banner.banner.map((item, i) =>
+            <div
+              className={className({
+                'carousel-item background_bg image-banner cursor-pointer': true,
+                'active': i === 0,
+                // 'cursor-pointer': item?.url
+              })}
+              onClick={(e) => handleClick(e, item.url)}>
+              <img src={`${imageUtils.getBannerUrl(item.image, enumType.imagePath.Banner)}`} />
+            </div>
+          )}
         </div>
-        <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev"><i className="ion-chevron-left" /></a>
-        <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next"><i className="ion-chevron-right" /></a>
-      </div>
+        <a className="carousel-control-prev border-radius-100" href="#carouselExampleControls" role="button" data-slide="prev"><i className="ion-chevron-left" /></a>
+        <a className="carousel-control-next border-radius-100" href="#carouselExampleControls" role="button" data-slide="next"><i className="ion-chevron-right" /></a>
+      </div >
     </div >
   )
 }
 
-export default BannerHome 
+export default BannerHome
