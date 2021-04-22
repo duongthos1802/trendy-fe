96b9c52c37aa4aa70e47b0c64e08296e78d39018
duongth <duongth@thnaglongtcd.com>
2021-04-22 16:22:40 +0700

    fix

--- src/components/GenerateSeo.js
+++ src/components/GenerateSeo.js
@@ -12,7 +12,7 @@ const defaultTitle = `${DEFAULT_SITE} - Chuyên dùng cho pha chế`
 
 const defaultDescription = ``
 
-const defaultImage = require('../static/assets/images/default/seo.jpeg')
+const defaultImage = 'https://api.nguyenlieutrendy.com/uploads/banner_pictures/1616035102665-banner-trendy.jpg'
 
 const GenerateSeo = ({ title, description, image, url, product, recipe }) => {
   // generate title
